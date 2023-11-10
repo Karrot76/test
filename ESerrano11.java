990141bc2616ccd65983d513d8a5d0b0385314e5
Erik Serrano <erikserrano76@gmail.com>
2023-11-10 02:28:57 +0000

    Java print name program

---
+++ ESerrano11.java
@@ -0,0 +1,8 @@
+//Erik Serrano
+//CSC-321
+//Lab 11
+public class ESerrano11{
+        public static void main(String[] args){
+                System.out.println("Hello, my name is Erik");
+        }
+}
